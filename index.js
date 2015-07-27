3ae52b8149dcbf1db8ad31a4c4861e3348723435
Kevin Lanni <therealklanni@gmail.com>
2015-07-26 22:59:59 -0700

    add dir for icon

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var menubar = require('menubar')
 var hotelClerk = menubar({
   index: 'http://localhost:2000',
+  dir: __dirname + '/app',
   'node-integration': false,
   height: 300,
   width: 364
