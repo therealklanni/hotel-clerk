ea9ceec6aab6c7ccb4ba6b13881d865e38d15467
Tobi Schäfer <interface@p3k.org>
2016-05-31 21:40:55 +0200

    chg: removed obsolete code

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const hotelClerk = menubar({
   dir: __dirname + '/app',
   height: 300,
   width: 364,
-  'node-integration': true,
   'preload-window': true
 });
 
@@ -18,43 +17,4 @@ hotelClerk.on('after-create-window', function () {
     event.preventDefault();
     require('shell').openExternal(url);
   });
-
-  /*
-  const debugEvent = function (name) {
-    html.on(name, function () {
-      console.info(name);
-    })
-  };
-
-  debugEvent('did-finish-load');
-  debugEvent('did-fail-load');
-  debugEvent('did-frame-finish-load');
-  debugEvent('did-start-loading');
-  debugEvent('did-stop-loading');
-  debugEvent('did-get-response-details');
-  debugEvent('did-get-redirect-request');
-  debugEvent('dom-ready');
-  debugEvent('page-favicon-updated');
-  debugEvent('new-window');
-  debugEvent('will-navigate');
-  debugEvent('did-navigate');
-  debugEvent('did-navigate-in-page');
-  debugEvent('crashed');
-  debugEvent('plugin-crashed');
-  debugEvent('destroyed');
-  debugEvent('devtools-opened');
-  debugEvent('devtools-closed');
-  debugEvent('devtools-focused');
-  debugEvent('certificate-error');
-  debugEvent('select-client-certificate');
-  debugEvent('login');
-  debugEvent('found-in-page');
-  debugEvent('media-started-playing');
-  debugEvent('media-paused');
-  debugEvent('did-change-theme-color');
-  debugEvent('cursor-changed');
-  debugEvent('context-menu');
-
-  html.openDevTools({mode: 'undocked'});
-  */
 });
