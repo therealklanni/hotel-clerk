325eb240e865dedb859bca555f48de41cbe90748
Tobi Schäfer <interface@p3k.org>
2016-06-02 20:48:06 +0200

    fix: open hotel links in browser window (#5)
    * fix: open hotel links in browser window
    
    * chg: radically improved code
    
    it was there the entire time but i clearly did not see it!
    
    * chg: removed obsolete code
    
    * fix: restored original node-integration setting

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 'use strict'
 
-var menubar = require('menubar')
-var hotelClerk = menubar({
+const menubar = require('menubar')
+
+const hotelClerk = menubar({
   index: 'http://localhost:2000',
   dir: __dirname + '/app',
-  'node-integration': false,
   height: 300,
-  width: 364
-})
+  width: 364,
+  'node-integration': false,
+  'preload-window': true
+});
+
+hotelClerk.on('after-create-window', function () {
+  const html = hotelClerk.window.webContents;
+
+  html.on('will-navigate', function (event, url) {
+    event.preventDefault();
+    require('shell').openExternal(url);
+  });
+});
