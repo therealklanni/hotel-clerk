ffcc51bcb1d96c046bf130fd5e328b67b69aeb27
Anthony Fu <anthonyfu117@hotmail.com>
2020-03-18 08:22:05 +0800

    fixes (#11)
    - Add quit context menu
    - Fix opening url in default browser for "new-window" event
    - Update deps

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 'use strict'
 
-const menubar = require('menubar')
+const { app, shell, Menu, Tray } = require('electron');
+const path = require('path');
+const { menubar } = require('menubar');
 
-const hotelClerk = menubar({
-  index: 'http://localhost:2000',
-  dir: __dirname + '/app',
-  height: 300,
-  width: 364,
-  'node-integration': false,
-  'preload-window': true
-});
+const iconPath = path.join(__dirname, 'app', 'IconTemplate.png');
 
-hotelClerk.on('after-create-window', function () {
-  const html = hotelClerk.window.webContents;
+app.on('ready', () => {
+  const tray = new Tray(iconPath);
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Quit', role: 'quit', click: () => app.exit() },
+  ]);
+  tray.addListener('right-click', () => tray.popUpContextMenu(contextMenu))
+  
+  const hotelClerk = menubar({
+    tray,
+    index: 'http://localhost:2000',
+    browserWindow: {
+      width: 350,
+      height: 350,
+    },
+    preloadWindow: true,
+  });
 
-  html.on('will-navigate', function (event, url) {
-    event.preventDefault();
-    require('shell').openExternal(url);
+  hotelClerk.on('after-create-window', function () {
+    const webContents = hotelClerk.window.webContents;
+
+    const handleRedirect = (e, url) => {
+      if (url != webContents.getURL()) {
+        e.preventDefault()
+        shell.openExternal(url)
+      }
+    };
+    
+    webContents.on('will-navigate', handleRedirect);
+    webContents.on('new-window', handleRedirect);
   });
 });
